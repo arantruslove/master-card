9f7ddab3920643145422bf9d6e2ca808ee39631d
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-06-02 15:23:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,30 @@
 // Importing modules
 import { get_rand_int, target, compare } from "./test_file.js";
 
-// Prime number test function
-function isPrime(num) {
-    // 1 is not a prime number
-    if (num <= 1) {
-      return false;
-    }
-    
-    // 2 and 3 are prime numbers
-    if (num === 2 || num === 3) {
-      return true;
-    }
-  
-    // Check if the number is divisible by any number from 2 to its square root
-    const limit = Math.sqrt(num);
-    for (let i = 2; i <= limit; i++) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    
-    // If the loop completes without finding a divisor, the number is prime
-    return true;
+// Function that enters the value of the button that has been pressed into
+// one of the inputs.
+function val_inputter(buttonID){
+  const button = document.getElementById(buttonID);
+  for (let i = 1; i < 5; i++){
+    const input = document.getElementById(`input${i}`);
+    let value = input.value;
+    if (value.trim() == "") {
+    input.value = button.value;
+    return null;
   }
+}
+}
 
-// End of prime number test function
+// Function that clears all the inputs.
+
+function inputs_clear(){
+  input1.value = '';
+  input2.value = '';
+  input3.value = '';
+  input4.value = '';
+}
 
 // Execute code if the button in page 3 is pressed:
-
 let target_cards;
 let num_deals = 0;
 const waterBottle = document.getElementById("deal");
@@ -37,6 +33,7 @@ waterBottle.addEventListener("click",function(){
     num_deals += 1;
     target_cards = target();
     document.getElementById("numberDeals").innerHTML = num_deals;
+    waterBottle.style.backgroundColor = 'grey';
 });
 
 // Function that compares the input cards to the target cards.
@@ -50,8 +47,41 @@ iceCream.addEventListener("click",function(){
     let card_store = [input1,input2,input3,input4];
     let test_result = compare(card_store,target_cards);
     document.getElementById("numberCorrect").innerHTML = test_result;
-
+    inputs_clear();
 });
+
+// Button interactivity
+
+// const button2 = document.getElementById('button2');
+// button2.addEventListener("click",function(){
+//   val_inputter('button2');
+// })
+
+// const button3 = document.getElementById('button3');
+// button3.addEventListener("click", function() {
+//   val_inputter('button3');
+// });
+
+for (let k = 2; k <= 10; k++) {
+  const button = document.getElementById(`button${k}`);
+  button.addEventListener("click", function() {
+    val_inputter(`button${k}`);
+  });
+}
+
+const specialValues = ['J', 'K', 'Q', 'A'];
+
+for (let i = 0; i < specialValues.length; i++) {
+  const value = specialValues[i];
+
+  const button = document.getElementById(`button${value}`);
+  button.addEventListener("click", function() {
+    val_inputter(`button${value}`);
+  });
+}
+
+
+
 
 
 
